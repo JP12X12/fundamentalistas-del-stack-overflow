4ce40936b9dfaddcbec347600f635670f9767a76
nahue <nginer@fi.uba.ar>
2024-07-18 19:30:07 -0300

    main version1

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,19 @@
-print "hola mundo"
+from flask import Flask, request, jsonify
+from models import db, Cliente, Habitacion, Hotel, Reserva
+
+app = Flask(__name__)
+port = 5000
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://fundamentalistas:fundamentalistas@localhost:5432/intro'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+if __name__ == '__main__':
+    print('arrancando')
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    print('arrancado ..')
+    app.run(host='0.0.0.0', debug=True, port=port)
