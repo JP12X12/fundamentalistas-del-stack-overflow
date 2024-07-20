ca68d7db7b91d0e77bdf2666e190341e79f59d11
jpformento <formento.juanp@gmail.com>
2024-07-20 19:14:38 -0300

    nuevo menu de inicio

--- Backend/app.py
+++ Backend/app.py
@@ -3,7 +3,7 @@ from models import db, Cliente, Habitacion, Hotel, Reserva
 
 app = Flask(__name__)
 port = 5000
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://fundamentalistas:fundamentalistas@localhost:5432/intro'
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://fundamentalistas:fundamentalistas@localhost:5000/intro'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 @app.route('/')
@@ -19,4 +19,30 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=port)
 
 
+@app.route('/registrar_cliente', methods=['POST'])
+def registrar_cliente():
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    dni = request.form['DNI']
+    telefono = request.form['telefono']
+    cant_dias = request.form['cant_dias']
+    edad = request.form['edad']
 
+    nuevo_cliente = Cliente(
+        nombre=nombre,
+        apellido=apellido,
+        DNI=dni,
+        telefono=telefono,
+        cant_dias=cant_dias,
+        edad=edad
+    )
+
+    db.session.add(nuevo_cliente)
+    db.session.commit()
+
+    return jsonify({'mensaje': 'Cliente registrado con éxito'})
+
+fetch("http://localhost:5000)
+    .then(response_received)
+    .then(parse_data)
+    .catch(request_error);
