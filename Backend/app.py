b8513e10ddea27e86ac753060e4fc35579b7e141
nahue <nginer@fi.uba.ar>
2024-07-20 18:48:04 -0300

    estilos t cambio en app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from models import db, Cliente, Habitacion, Hotel, Reserva
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 port = 5000
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://fundamentalistas:fundamentalistas@localhost:5432/intro'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 @app.route('/')
-def hello_world():
-    return 'Hola mundo!'
+def Home():
+    return 'Hola que tal'
 
 if __name__ == '__main__':
     print('arrancando')
